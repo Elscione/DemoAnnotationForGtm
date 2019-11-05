f4515b239b5e1a3f74600bdd38b5948711dd8938
Yusuf Hendrawan <yusuf.hendrawan@tokopedia.com>
2019-11-05 16:21:12 +0700

    Initial commit

---
+++ processor/src/main/java/com/example/processor/AnnotationProcessor.kt
@@ -0,0 +1,24 @@
+package com.example.processor
+
+import com.google.auto.service.AutoService
+import javax.annotation.processing.AbstractProcessor
+import javax.annotation.processing.Processor
+import javax.annotation.processing.RoundEnvironment
+import javax.lang.model.SourceVersion
+import javax.lang.model.element.TypeElement
+
+@AutoService(Processor::class)
+class AnnotationProcessor : AbstractProcessor() {
+    override fun process(elements: MutableSet<out TypeElement>?, roundEnv: RoundEnvironment?): Boolean {
+
+        return true
+    }
+
+    override fun getSupportedSourceVersion(): SourceVersion {
+        return SourceVersion.latest()
+    }
+
+    override fun getSupportedAnnotationTypes(): MutableSet<String> {
+        return super.getSupportedAnnotationTypes()
+    }
+}
